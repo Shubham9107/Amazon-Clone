c99f9094cff1dc79431aab0133edfe7685c81401
Shubham Joshi <42163609+Shubham9107@users.noreply.github.com>
2024-05-13 14:55:48 +0530

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import FooterBottom from './FooterBottom'
 import FooterMiddle from './FooterMiddle'
-import FooterTop from './FooterTop'
 
 const Footer = () => {
   return (
     <div className='font-titleFont'>
-        <FooterTop />
+        
         <FooterMiddle />
         <FooterBottom />
     </div>
