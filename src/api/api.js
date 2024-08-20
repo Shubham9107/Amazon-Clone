3bfa84efe3d0b451a9874d81b24c2455a3753a09
Shubham Joshi <42163609+Shubham9107@users.noreply.github.com>
2024-08-20 21:08:17 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function productData() {
-    const products = await axios.get("https://fakedata.vercel.app/");
+    const products = await axios.get("https://fakestoreapi.com/products");
     return products
 }
